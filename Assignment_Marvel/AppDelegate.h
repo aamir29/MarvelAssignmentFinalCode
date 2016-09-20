a1777481ccc29d8a2b1ba1823d71aad1f5e0f6b9
AvisBudget <avisbudget@aamir-mac-mini.local>
2016-09-20 14:37:21 +0530

    Initial Commit

---
+++ Assignment_Marvel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment_Marvel
+//
+//  Created by AvisBudget on 9/20/16.
+//  Copyright © 2016 AvisBudget. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assignment_Marvel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assignment_Marvel
+//
+//  Created by AvisBudget on 9/20/16.
+//  Copyright © 2016 AvisBudget. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
